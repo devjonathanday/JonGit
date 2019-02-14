9b9b091c3da4a59f9e12f32dd724864809be4170
Jonathan Day <j.day151r@gmail.com>
2019-02-14 10:00:04 -0800

    Why can't I start another goddamn process reeeee

--- WinGit/MainWindow.xaml.cs
+++ WinGit/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Diagnostics;
 
 namespace WinGit
 {
@@ -22,22 +24,43 @@ namespace WinGit
     {
         public string workingDir;
         public string bashDir;
+        public StringWriter sw = new StringWriter();
+        Process bash = new Process();
+        //public ProcessStartInfo bash;
 
         public MainWindow()
         {
             InitializeComponent();
+            ResizeMode = ResizeMode.NoResize;
             workingDir = System.IO.Directory.GetCurrentDirectory();
-            bashDir = workingDir + "\\Git\\git-cmd.exe";
+            bashDir = bashDirTextBox.Text;
+            bash.StartInfo.FileName = bashDir;
+            bash.Start();
         }
 
-        private void StartBash(object sender, RoutedEventArgs e)
+        private void InputArgs(string newArgs)
         {
-            System.Diagnostics.Process.Start(bashDir);
+            //Process[] currentProcesses = Process.GetProcesses();
+            //foreach (Process p in currentProcesses)
+            //    OutputText.Text += p.ProcessName;
+            //bash.StartInfo.FileName = bashDir;
+            //bash.StartInfo.Arguments = newArgs;
+            bash.Start();
         }
 
-        private void InputToGit(string command)
+        private void CleanUpGit(object sender, System.ComponentModel.CancelEventArgs e)
         {
+            //bash.Kill();
+        }
 
+        private void GitStatus(object sender, RoutedEventArgs e)
+        {
+            InputArgs("git status");
+        }
+
+        private void ChangeDir(object sender, RoutedEventArgs e)
+        {
+            InputArgs("cd ..");
         }
     }
 }
