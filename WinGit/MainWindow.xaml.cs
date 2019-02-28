5b88c4ccd4a5ceaab3d7f6416788960d23d0f40e
Jonathan Day <j.day151r@gmail.com>
2019-02-28 12:17:29 -0800

    Added recent repos list

--- WinGit/MainWindow.xaml.cs
+++ WinGit/MainWindow.xaml.cs
@@ -1,18 +1,18 @@
-﻿//using System;
-//using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.IO;
-//using System.Linq;
-//using System.Text;
-//using System.Threading.Tasks;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using System.Windows;
-//using System.Windows.Controls;
-//using System.Windows.Data;
-//using System.Windows.Documents;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
 using System.Windows.Input;
-//using System.Windows.Media;
-//using System.Windows.Media.Imaging;
-//using System.Windows.Navigation;
-//using System.Windows.Shapes;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
 using System.Diagnostics;
 using System.Windows.Forms;
 
@@ -55,7 +55,11 @@ namespace WinGit
         private void GitPushButton(object sender, RoutedEventArgs e)
         {
             GM.GitPush(repoDirText.Text);
-            RecentRepoDirsList.Items.Add(repoDirText.Text);
+            if (!RecentRepoDirsList.Items.Contains(repoDirText.Text))
+            {
+                RecentRepoDirsList.Items.Insert(0, repoDirText.Text);
+                while (RecentRepoDirsList.Items.Count > 3) RecentRepoDirsList.Items.RemoveAt(RecentRepoDirsList.Items.Count - 1);
+            }
         }
         private void BrowseForRepoDir(object sender, RoutedEventArgs e)
         {
