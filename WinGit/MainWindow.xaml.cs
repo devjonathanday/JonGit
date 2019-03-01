0e697c12473eff2603ce72ed2c661600d967fb57
Jonathan Day <j.day151r@gmail.com>
2019-02-28 16:00:03 -0800

    Fixed recent repo list

--- WinGit/MainWindow.xaml.cs
+++ WinGit/MainWindow.xaml.cs
@@ -32,12 +32,29 @@ namespace WinGit
             InitializeComponent();
             //ResizeMode = ResizeMode.NoResize;
             GM = new GitManager(ref OutputBlock, ref ReadoutScrollViewer, ref ReadoutBlock);
-            if(!File.Exists(recentReposFileName)) File.Create(recentReposFileName); //Creates the recent repo text file if it doesn't exist.
-            UpdateRecentRepos();
+            if (!File.Exists(recentReposFileName)) File.Create(recentReposFileName); //Creates the recent repo text file if it doesn't exist.
+            //UpdateRecentRepos();
         }
 
         //WPF Buttons
-        private void GitStatusButton(object sender, RoutedEventArgs e) { GM.GitStatus(repoDirText.Text); }
+        private void GitStatusButton(object sender, RoutedEventArgs e)
+        {
+            GM.GitStatus(repoDirText.Text);
+
+            //Update recent repositories
+            string[] repoArr = File.ReadAllLines(recentReposFileName); //Read repo names from file, put into an array
+            if (repoArr.Length == 0) { File.WriteAllText(recentReposFileName, repoDirText.Text); }
+            else
+            {
+                List<string> repoList = new List<string>(); //Convert array into list
+                repoList.Add(repoDirText.Text); //Add the current repo to the top of the list
+                for (int i = 0; i < Math.Min(repoArr.Length, 4); i++) //Iterate through the list up to 5 times
+                    if (repoArr[i] != repoDirText.Text) //Check for duplicates
+                        repoList.Add(repoArr[i]); //Add the rest of the repos into the list
+                File.WriteAllLines(recentReposFileName, repoList.ToArray()); //Convert back into an array for writing
+            }
+            UpdateRecentRepos(); //Update the combo box
+        }
         private void AddFileButton(object sender, RoutedEventArgs e) { GM.AddFileToCommit(repoDirText.Text); }
         private void AddAllButton(object sender, RoutedEventArgs e) { GM.AddAllFiles(repoDirText.Text); }
         private void RemoveFileButton(object sender, RoutedEventArgs e) { GM.RemoveFileFromCommit(repoDirText.Text); }
@@ -49,25 +66,7 @@ namespace WinGit
         private void OpenCMDButton(object sender, RoutedEventArgs e) { GM.OpenCMD(repoDirText.Text); }
         private void GitInitButton(object sender, RoutedEventArgs e) { GM.GitInit(repoDirText.Text); }
         private void AddOriginButton(object sender, RoutedEventArgs e) { GM.RemoteAddOrigin(WebOriginTextBox.Text, repoDirText.Text); }
-        private void GitPushButton(object sender, RoutedEventArgs e)
-        {
-            GM.GitPush(repoDirText.Text);
-            //string[] repoList = File.ReadAllLines(recentReposFileName); //Read repo names into an array
-            //string[] newRepoList = new string[5]; //Create new array 1 element larger
-            //newRepoList[0] = repoDirText.Text; //Set the first element of the new array
-            //for(int i = 1; i < Math.Min(5, repoList.Length); i++) //Iterate through the list, or just go to 5 if it is too large
-            //{
-            //    newRepoList[i] = repoList[]
-            //}
-
-            //if (!RecentRepoDirsList.Items.Contains(repoDirText.Text))
-            //{
-            //    //RecentRepoDirsList.Items.Insert(0, repoDirText.Text);
-            //    //while (RecentRepoDirsList.Items.Count > 3) RecentRepoDirsList.Items.RemoveAt(RecentRepoDirsList.Items.Count - 1);
-            //    //FileStream recentRepos = File.Open(recentReposFileName, FileMode.Open);
-                
-            //}
-        }
+        private void GitPushButton(object sender, RoutedEventArgs e) { GM.GitPush(repoDirText.Text); }
         private void BrowseForRepoDir(object sender, RoutedEventArgs e)
         {
             FolderBrowserDialog folderBrowser = new FolderBrowserDialog();
@@ -77,11 +76,13 @@ namespace WinGit
         private void GitPullButton(object sender, RoutedEventArgs e) { GM.GitPull(repoDirText.Text, WebOriginTextBox.Text); }
         private void SetUpstreamCheckBox_Unchecked(object sender, RoutedEventArgs e) { GM.setUpstream = false; }
         private void SetUpstreamCheckBox_Checked(object sender, RoutedEventArgs e) { GM.setUpstream = false; }
-        private void UpdateRepoDir(object sender, System.Windows.Controls.SelectionChangedEventArgs e) { repoDirText.Text = RecentRepoDirsList.SelectedItem.ToString(); }
+        private void UpdateRepoDir(object sender, System.Windows.Controls.SelectionChangedEventArgs e) { if(RecentRepoDirsList.SelectedItem != null) repoDirText.Text = RecentRepoDirsList.SelectedItem.ToString(); }
         public void UpdateRecentRepos() //Iterates through text file and updates ComboBox with recently pushed repos
         {
             string[] recentRepos = File.ReadAllLines(recentReposFileName); //Read the repo names and put them into a string array.
+            RecentRepoDirsList.Items.Clear();
             for (int i = 0; i < recentRepos.Length; i++) RecentRepoDirsList.Items.Add(recentRepos[i]); //Add the repo names into the combo box.
         }
+        private void FocusedRecentsBox(object sender, RoutedEventArgs e) { UpdateRecentRepos(); }
     }
 }
